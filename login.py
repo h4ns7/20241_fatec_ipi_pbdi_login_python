26e8c64bca32ed00715107eb6109b2f645d3a82a
Aluno <aluno@IPIRANGA.FATEC>
2024-03-12 09:57:11 -0300

    Apostila07(git-python):funcao insercao de novos usuarios

--- login.py
+++ login.py
@@ -29,7 +29,51 @@ def existe(usuario):
       #return True if result != None else False
       return result != None
     
-def teste():
-  print(existe(Usuario('admin', 'admin')))
+#def teste():
+# print(existe(Usuario('admin', 'admin')))
+#
+#teste()
+    
+def adicionar_usuario(usuario):
+  with psycopg.connect(
+    host="localhost",
+    port=5432,
+    dbname="20241_fatec_ipi_pbdi_login_python",
+    user="postgres",
+    password="123456"
+  ) as conexao:
+    with conexao.cursor() as cursor:
+      cursor.execute('INSERT INTO tb_usuario (login, senha) VALUES (%s, %s)',
+                     (usuario.login, usuario.senha))
+      conexao.commit()
+      print("usuario adicionado com sucesso")
 
-teste()
+
+
+def menu ():
+  texto = ' 0-Sair\n 1-Login\n 2-Logoff\n 3-Novo usuário\n'
+  #usuario ainda não existe
+  usuario = None
+  op = int(input(texto))
+  while op != 0:
+      if op ==1:
+        login = input("Digite o login")
+        senha = input("Digite a senha")
+        usuario = Usuario(login, senha)
+        #expressão condicional (if/else de uma linha só)
+        print('Usuário OK!' if existe(usuario) else 'Usuário NOK')
+        pass
+      elif op == 2:
+        usuario = None
+        print("Logoff realizado com sucesso")
+      elif op ==3:
+        login =input("Digite o novo login:")
+        senha = input("Digite a nova senha")
+        novo_usuario = Usuario (login, senha)
+        adicionar_usuario(novo_usuario)
+      else: 
+        print('Digite apenas numeros entre 0 e 3')
+      op = int(input(texto))
+
+
+menu()
